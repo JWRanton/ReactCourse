f620bb3a1cabb2c087a24d22e4917f14c08b5093
Ranton <jranton@MO01C02RM0YEFVL.local>
2016-06-27 10:40:34 -0500

    Nesting components part 1

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,27 @@
-// simple component
+
+// Message component
+var GreeterMessage = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Some H1</h1>
+        <p>Some Paragraph</p>
+      </div>
+    );
+  }
+});
+// Form component
+var GreeterForm = React.createClass({
+  render: function () {
+    return (
+      <form>
+        <input type="text" ref="name"/>
+        <button>Set Name</button>
+      </form>
+    );
+  }
+});
+// Hello component
 var Greeter = React.createClass({
     getDefaultProps: function() {
         return {name: 'React', message: 'This is from the component.'};
@@ -25,10 +48,15 @@ var Greeter = React.createClass({
             <div>
                 <h1>Hello {name}!</h1>
                 <p>{message}</p>
+
+                <GreeterMessage/>
+
                 <form onSubmit={this.onButtonClick}>
                     <input type="text" ref="name"/>
                     <button>Set Name</button>
                 </form>
+
+                <GreeterForm/>
             </div>
         );
     }
