530147d42cbfe902e349a0682743760f729fd961
Ranton <jranton@MO01C02RM0YEFVL.local>
2016-06-27 15:06:11 -0500

    added updates and finished nesting

--- public/app.jsx
+++ public/app.jsx
@@ -17,46 +17,63 @@ var GreeterForm = React.createClass({
     onFormSubmit: function(e) {
         e.preventDefault();
 
+        var updates = {};
         var name = this.refs.name.value;
+        var message = this.refs.message.value;
 
         if (name.length > 0) {
             this.refs.name.value = '';
-            this.props.onNewName(name);
-
+            updates.name = name;
         }
+
+        if (message.length > 0) {
+          this.refs.message.value = '';
+          updates.message = message;
+        }
+
+        this.props.onNewData(updates);
     },
     render: function() {
         return (
             <form onSubmit={this.onFormSubmit}>
-                <input type="text" ref="name"/>
-                <button>Set Name</button>
+              <div>
+                <input type="text" ref="name" placeholder="enter name"/>
+              </div>
+              <div>
+                <textarea ref="message" placeholder="enter message"></textarea>
+              </div>
+              <div>
+                <button>Submit</button>
+              </div>
             </form>
         );
     }
 });
-
-// Hello component
+// Greeter component
 var Greeter = React.createClass({
     getDefaultProps: function() {
         return {name: 'React', message: 'This is from the component.'};
     },
     getInitialState: function() {
-        return {name: this.props.name};
+        return {
+          name: this.props.name,
+          message: this.props.message
+        };
     },
-    handleNewName: function(name) {
-        this.setState({name: name});
+    handleNewData: function(updates) {
+        this.setState(updates);
+
     },
     render: function() {
         var name = this.state.name;
-        var message = this.props.message;
+        var message = this.state.message;
         return (
             <div>
                 <GreeterMessage name={name} message={message}/>
-                <GreeterForm onNewName={this.handleNewName}/>
+                <GreeterForm onNewData={this.handleNewData}/>
             </div>
         );
     }
 });
-
 ReactDOM.render(
     <Greeter/>, document.getElementById('app'));
